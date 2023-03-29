f3106777b35e504eba3240fb412fafd029e13ea8
Andrey Kruglov <kruglov.it.rs@gmail.com>
2023-03-29 23:24:30 +0300

    step 1

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,18 @@
 import { Home } from "./pages/home";
 import { Login } from "./pages/login";
 import { Settings } from "./pages/settings";
+import {useState} from "react";
 
 const App = () => {
+  const [url, setUrl] = useState("");
+
   return (
-      <>
-        <Home />
-        <Login />
-        <Settings />
-      </>
+      <div className="content">
+          <input value={url} onChange={e => setUrl(e.target.value)} />
+          {url === "/home" && <Home /> }
+          {url === "/login" && <Login /> }
+          {url === "/settings" && <Settings /> }
+      </div>
   );
 };
 
